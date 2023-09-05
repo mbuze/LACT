02e03690ad8967edc366c9a390a92fa625f41504
mbuze <maciej.buze@gmail.com>
2023-09-05 15:29:21 +0100

    towards working continuation code vol 1

---
+++ LACT/__init__.py
@@ -0,0 +1,7 @@
+from .utils import *
+
+# __all__ = ["geometry_utils",
+#            "plotting_utils",
+#            "pixel_method",
+#            "esdot_method"]
+
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LACT',
+    version='0.0.1',
+    description='LAmmps Continuation Techniques. A python-based wrapper around LAMMPS implementing numerical continuation algorithms',
+    packages=find_packages()
+)
